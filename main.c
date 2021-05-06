f78c31ca1cdabc774a2f06fa12ef8fd0d942766c
MartijnGuilliamsPXL <martijn.guilliams@student.pxl.be>
2021-05-06 15:44:27 +0200

    +++grootte uitlezen en aanpssen naar 8bits binair

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* 
 * to do:
-* grootte uitlezen 
+* +++grootte uitlezen en aanpssen naar 8bits binair
 * 
 * 
 * 
@@ -19,28 +19,71 @@ void printbinchar(char);
 
 int main()
 {
-    printbinchar(' ');
+    printbinchar('t');
     return 0;
 }
+
 void printbinchar(char character)
 {
-    char test[9] = "";
-    char output[9];
-    _itoa(character, output, 2);
-    printf("%s\n", output);
+    char binLetter_8[9] = "";
+    char binLetter[9];
+    _itoa(character, binLetter, 2); //character omzetten naar 2(binair) in binLetter
+    printf("%s\n", binLetter); //printen binaire waarde van letter meegegeven aan printbinchar();
 
-    int i = 0;
-    while (output[i] != '\0') i++;
-    printf("%d\n", i);
+    int aantalBits = 0;
+    while (binLetter[aantalBits] != '\0') aantalBits++;
+    printf("Aantal bits: %d\n", aantalBits);
 
-
-    if(i < 8) {
-        for (int j = 0; j < (8 - i); j++)
+    //omzetten naar 8 bits binair
+    if(aantalBits < 8) {
+        for (int j = 0; j < (8 - aantalBits); j++)
         {
-            test[j] = '0';
+            binLetter_8[j] = '0';
         }
     }
-    strcat(test, output);
-    printf("%s\n", test);
+    strcat(binLetter_8, binLetter);
+    printf("Omgezet in 8 bits binair: %s\n", binLetter_8);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+typedef struct _iobuf
+
+{
+
+    char* _ptr;
+
+    int _cnt;
+
+    char* _base;
+
+    int _flag;
+
+    int _file;
+
+    int _charbuf;
+
+    int _bufsiz;
+
+    char* _tmpfname;
+
+} FILE;
+
+unix.com/programming/150687-file-structure-stdio-h.html
+
+tutorialspoint.com / cprogramming / c_bit_fields.htm
+
+*/

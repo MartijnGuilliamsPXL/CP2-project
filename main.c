3b7ad0cd5f35e40845384daa8e26dc73277bb7ab
MartijnGuilliamsPXL <martijn.guilliams@student.pxl.be>
2021-05-06 17:38:23 +0200

    +++cmd argumenten kunnen uitlezen  +++txt bestand uitlezen

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 /* 
 * to do:
 * +++grootte uitlezen en aanpssen naar 8bits binair
+* +++cmd argumenten kunnen uitlezen
+* +++txt bestand uitlezen
+* ---tekst omvormen naar binair
+* ---grootte van bmp bestand uitlezen
+* ---tekst in bmp bestand verwerken
+* ---bmp bestand uitlezen
+* ---binair omzetten naar tekst
 * 
-* 
-* 
-* 
-* 
-* 
+* ---fout geven bij te weinig of foutive cmd argumenten
 */
 
 
@@ -14,11 +17,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define BESTAND "./test.txt"
 
 void printbinchar(char);
 
-int main()
-{
+int main(int argc, char* argv[]) {
+	if(argc == 2 && strcmp(argv[1], "--help")==0)
+    {
+		printf("-c --> compress\n");
+		printf("-s --> de input text file containing the secret message\n");
+		printf("-i --> de input bmp file\n");
+		printf("-o --> output image name\n");
+		printf("EINDE HELP\n");
+	}
+
+	if(argc == 8)
+    {
+		//-c -s inputfile.txt -i meme.bmp -o memeOut.bmp
+		printf("test\n");
+		printf("%s\n", argv[3]);
+		printf("%s\n", argv[5]);
+		printf("%s\n", argv[7]);
+	}
+
+
+
+    FILE * fp = NULL; 
+
+    fp = fopen(BESTAND, "r");
+    if (fp == NULL)
+    {
+        printf("Can't open file\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        int c;
+        while ((c = getc(fp)) != '#' && c != EOF)putchar(c);
+        printf("The contents of desname string  : %s", c);
+    }
+    
+
+    fclose(fp);
+    return 0;
+
+
+
+
+
+
+
+    /*
+    if (argc == 2) {
+        printf("The argument supplied is %s\n", argv[1]);
+    }
+    else if (argc > 2) {
+        printf("Too many arguments supplied.\n");
+    }
+    else {
+        printf("One argument expected.\n");
+    }
+    */
+
     printbinchar('t');
     return 0;
 }
@@ -28,7 +88,7 @@ void printbinchar(char character)
     char binLetter_8[9] = "";
     char binLetter[9];
     _itoa(character, binLetter, 2); //character omzetten naar 2(binair) in binLetter
-    printf("%s\n", binLetter); //printen binaire waarde van letter meegegeven aan printbinchar();
+    printf("Binaire waarde van letter: %s\n", binLetter); //printen binaire waarde van letter meegegeven aan printbinchar();
 
     int aantalBits = 0;
     while (binLetter[aantalBits] != '\0') aantalBits++;
@@ -84,6 +144,6 @@ typedef struct _iobuf
 
 unix.com/programming/150687-file-structure-stdio-h.html
 
-tutorialspoint.com / cprogramming / c_bit_fields.htm
+tutorialspoint.com / cprogramming / c_bit_fields.html
 
 */
